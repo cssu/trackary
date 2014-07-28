1170a7d10c02394febe76e39a5e439ecca047fb1
Spencer Elliott <me@elliottsj.com>
2014-07-27 23:51:38 -0400

    Use alternative domain model

--- db/migrate/20140728023145_create_purchases.rb
+++ db/migrate/20140728023145_create_purchases.rb
@@ -1,6 +1,9 @@
 class CreatePurchases < ActiveRecord::Migration
   def change
     create_table :purchases do |t|
+      t.integer :quantity
+      t.references :product, index: true
+      t.references :order, index: true
 
       t.timestamps
     end
