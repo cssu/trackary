6c5f6837f2160d140dfb0fbf9cc0df88c898d9fa
Spencer Elliott <me@elliottsj.com>
2014-08-14 23:06:32 -0400

    Add sign-in click handler

--- app/assets/components/pages/ty-signin-register/ty-signin-register.js
+++ app/assets/components/pages/ty-signin-register/ty-signin-register.js
@@ -2,7 +2,24 @@ Polymer('ty-signin-register', {
   ready: function () {
 
   },
+
   handleNewRegistration: function (registeredUser) {
 
+  },
+
+  onSignInClick: function () {
+    debugger;
+    var email = this.$.email.value;
+    var password = this.$.password.value;
+
+    this.$.devise.register({
+      email: email,
+      password: password,
+      password_confirmation: password
+    }).then(function (registeredUser) {
+      console.info('registeredUser:', registeredUser);
+    }, function (error) {
+      console.error(error);
+    });
   }
 });
