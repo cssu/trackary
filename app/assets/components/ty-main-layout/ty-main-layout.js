baee1c4f7327980c844e52528fdfdee7c4c22480
Spencer Elliott <me@elliottsj.com>
2014-08-18 21:45:42 -0400

    Add comments And make menu items more better

--- app/assets/components/ty-main-layout/ty-main-layout.js
+++ app/assets/components/ty-main-layout/ty-main-layout.js
@@ -2,15 +2,10 @@ Polymer('ty-main-layout', {
 
   domReady: function() {
     this.topLevelRoutes = this.$.router.getTopLevelRoutes();
-    this.activePath = this.$.router.activeRoute.hashPath;
   },
 
   handleRouteChange: function (event, route, sender) {
-    debugger;
     this.activeRoute = route;
-    this.$.menu.selected = _.findIndex(this.$.menu.querySelectorAll('a'), function (a) {
-      return a.getAttribute('href') === route.hashPath;
-    });
   }
 
 });
--- app/assets/components/ty-router/ty-router.js
+++ app/assets/components/ty-router/ty-router.js
@@ -3,16 +3,14 @@
   var _routes = [];
 
   Polymer('ty-router', {
-    routes: _routes,
 
-    get activeRoute() {
-      var appRoute = this.querySelector('::shadow app-route[active]');
-      if (!appRoute) return;
-      var path = appRoute.getAttribute('path');
-      return _.find(this.routes, function (route) {
-        return route.path === path;
-      });
-    },
+    /**
+     * Shared set of routes accessible from all instances of <ty-router>
+     *
+     * @property routes
+     * @type {array}
+     */
+    routes: _routes,
 
     ready: function() {
       _.forEach(this.querySelectorAll('ty-route'), function (route) {
@@ -21,16 +19,51 @@
       this.initialized = true;
     },
 
+    /**
+     * Get the set of top-level routes (i.e. <ty-route topLevel="true">)
+     *
+     * @returns {Element[]}
+     */
     getTopLevelRoutes: function () {
       return _.filter(this.routes, function (route) {
         return route.topLevel === 'true';
       });
     },
 
-    handleStateChange: function () {
-      this.async(function () {
-        this.asyncFire('route-change', this.activeRoute);
-      }, null, 0);
+    /**
+     * Gets the active route.
+     *
+     * @returns {Element|null} The active <ty-route> element, or null if there is none
+     */
+    getActiveRoute: function () {
+      var appRoute = this.querySelector('::shadow app-route[active]');
+      if (!appRoute) return null;
+      var path = appRoute.getAttribute('path');
+      return _.find(this.routes, function (route) {
+        return route.path === path;
+      });
+    },
+
+    /**
+     * Gets the <ty-route> with the given path
+     *
+     * @param {string} path
+     * @returns {Element|null} The <ty-route> with the given path, or null if none exists
+     */
+    getRouteWithPath: function (path) {
+      return _.find(this.routes, function (route) {
+        return route.path === path;
+      });
+    },
+
+    /**
+     * Handle when the underlying <app-router> changes its route.
+     *
+     * @param event
+     * @param route
+     */
+    handleStateChange: function (event, route) {
+      this.fire('route-change', this.getRouteWithPath(route.path));
     }
 
   });
