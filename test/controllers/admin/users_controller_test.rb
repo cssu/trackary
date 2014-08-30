d105213deaeb7d2fffe3d343239945851a7bfaaf
Spencer Elliott <me@elliottsj.com>
2014-08-30 14:57:25 -0400

    Test stubs

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -1,7 +1,58 @@
 require 'test_helper'
 
-class Admin::UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Admin::UsersController do
+
+  it 'should allow admins to view the list of users' do
+    user =
+    get :index
+    assert_response :success
+  end
+
+  it 'should allow admins to access the new user creation page' do
+    get :new
+    assert_response :success
+  end
+
+  it 'should allow admins to create a new user with an email, password, and password confirmation' do
+    get :create
+    assert_response :success
+  end
+
+  it 'should allow admins to access the user editing page' do
+    get :edit
+    assert_response :success
+  end
+
+  it 'should allow admins to view individual users' do
+    get :show
+    assert_response :success
+  end
+
+  it 'should allow admins to update a user with a new email and password (with password confirmation)' do
+    get :update
+    assert_response :success
+  end
+
+  it 'should allow admins to update a user with just a new email' do
+    get :update
+    assert_response :success
+  end
+
+  it 'should allow admins to update a user with just a new password (with password confirmation)' do
+    get :update
+    assert_response :success
+  end
+
+  it 'should allow admins to delete a user' do
+    get :destroy
+    assert_response :success
+  end
+
+
+  it 'should redirect non-admins to the previous page with a flash[:alert] message' do
+    get :index
+    assert_response :success
+
+  end
+
 end
