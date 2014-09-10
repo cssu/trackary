ec0d8f1cb4bf45b5d31cbf4be05c309565227936
Spencer Elliott <me@elliottsj.com>
2014-09-09 23:33:32 -0400

    No more user_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@
 # Functions available in all application templates
 module ApplicationHelper
 
-  def user_link
-    if user_signed_in?
-      link_to 'Account', edit_user_registration_path
-    elsif
-      link_to 'Sign In / Register', new_user_session_path
-    end
+  def current_user_label
+    user_signed_in? ? 'Account' : 'Sign In / Register'
+  end
+
+  def current_user_path
+    user_signed_in? ? edit_user_registration_path : new_user_session_path
   end
 
 end
